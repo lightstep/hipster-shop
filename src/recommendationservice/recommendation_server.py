e7a6fda4a5a0c06a5e3bf348e6c1a68509a3fdb8
Alex Boten <aboten@lightstep.com>
2020-02-26 12:58:23 -0800

    add propagation config to recommendationservice
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- src/recommendationservice/recommendation_server.py
+++ src/recommendationservice/recommendation_server.py
@@ -13,6 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from ddtrace import tracer
+from ddtrace.propagation.b3 import B3HTTPPropagator
+
+tracer.configure(http_propagator=B3HTTPPropagator)
 
 import os
 import random
