a4ca7e6281075b4d474ff92d95399aafdd2df400
Alex Boten <aboten@lightstep.com>
2020-02-28 13:21:53 -0800

    fixing missing product codes
    Signed-off-by: Alex Boten <aboten@lightstep.com>

--- src/adservice/src/main/java/hipstershop/AdService.java
+++ src/adservice/src/main/java/hipstershop/AdService.java
@@ -169,47 +169,44 @@ public final class AdService {
   }
 
   private static ImmutableListMultimap<String, Ad> createAdsMap() {
-    Ad camera =
+    Ad coffee =
         Ad.newBuilder()
-            .setRedirectUrl("/product/2ZYFJ3GM2N")
-            .setText("Film camera for sale. 50% off.")
+            .setRedirectUrl("/product/OLJCESPC7Z")
+            .setText("Coffee for sale. 50% off.")
             .build();
-    Ad lens =
+    Ad rainbowDelight =
         Ad.newBuilder()
             .setRedirectUrl("/product/66VCHSJNUP")
-            .setText("Vintage camera lens for sale. 20% off.")
+            .setText("Rainbow delight sale. 20% off.")
             .build();
-    Ad recordPlayer =
+    Ad chocolateSupreme =
         Ad.newBuilder()
             .setRedirectUrl("/product/0PUK6V6EV0")
-            .setText("Vintage record player for sale. 30% off.")
+            .setText("Chocolate supreme sale. 30% off.")
             .build();
-    Ad bike =
+    Ad oldFashioned =
         Ad.newBuilder()
-            .setRedirectUrl("/product/9SIQT8TOJO")
-            .setText("City Bike for sale. 10% off.")
+            .setRedirectUrl("/product/3R92ZDDYKL")
+            .setText("Old fashioned sale. 10% off.")
             .build();
     Ad baristaKit =
         Ad.newBuilder()
             .setRedirectUrl("/product/1YMWWN1N4O")
-            .setText("Home Barista kitchen kit for sale. Buy one, get second kit for free")
+            .setText("Home barista kitchen kit for sale. Buy one, get second kit for free")
             .build();
-    Ad airPlant =
+    Ad metalCampingMug =
         Ad.newBuilder()
-            .setRedirectUrl("/product/6E92ZMYYFZ")
-            .setText("Air plants for sale. Buy two, get third one for free")
+            .setRedirectUrl("/product/LS4PSXUNUM")
+            .setText("Metal camping mug for sale. Buy two, get third one for free")
             .build();
-    Ad terrarium =
+    Ad cookieCrumble =
         Ad.newBuilder()
             .setRedirectUrl("/product/L9ECAV7KIM")
-            .setText("Terrarium for sale. Buy one, get second one for free")
+            .setText("Cookie crumble sale. Buy one, get second one for free")
             .build();
     return ImmutableListMultimap.<String, Ad>builder()
-        .putAll("photography", camera, lens)
-        .putAll("vintage", camera, lens, recordPlayer)
-        .put("cycling", bike)
-        .put("cookware", baristaKit)
-        .putAll("gardening", airPlant, terrarium)
+        .putAll("donuts", rainbowDelight, chocolateSupreme, oldFashioned, cookieCrumble)
+        .putAll("coffee", coffee, baristaKit, metalCampingMug)
         .build();
   }
 
